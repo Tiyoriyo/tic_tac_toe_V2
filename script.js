a9c9690b915405a7485a50f8ff44d3249823b827
Vadim Kostrov <vmkvo15@outlook.com>
2023-04-04 18:20:44 +0400

    Add Player Select Game Selection screen

--- script.js
+++ script.js
@@ -1,3 +1,135 @@
+const Intro = (() => {
+    const Body = document.querySelector('body');
+
+    const GameTypeSelect = () => {
+        Body.innerHTML = `
+            <div class = 'GameTitle'>Tic Tac Toe</div>
+            <div class = 'ButtonSet'>
+                <button class = 'PvPBtn'>Player</button>
+                <button class = 'CPUBtn'>Computer</button>
+            </div>
+        `
+    
+        const Buttons = document.querySelectorAll('button');
+        Buttons.forEach(function(x) {
+            x.addEventListener('click', (e) => {
+                if (e.target.className == 'PvPBtn') {
+                    Body.innerHTML = '';
+                    SetNameRender('PvP');
+                } else if (e.target.className == 'CPUBtn') {
+                    Body.innerHTML = '';
+                    SetNameRender('CPU');
+                }
+            });
+        });
+    };
+
+    GameTypeSelect();    
+
+    const SetNameRender = (type) => {
+        if (type == 'PvP') {
+            Body.innerHTML = `
+            <div class = 'GameTitle'>Tic Tac Toe</div>
+            <div class = 'InputFieldContainer'>
+                <div class ='InputField'>
+                    <div class ='InputTitle'>Player 1</div>
+                    <div class = 'TeamFieldContainer'>
+                        <div class ='TeamField'>
+                            <div class = 'TeamSign'>X</div>
+                            <input type = 'radio' id = 'x' name = 'Ply1GameType'></radio>
+                        </div>
+                        <div class ='TeamField'>
+                            <div class ='TeamSign'>Y</div>
+                            <input type = 'radio' id = 'y' name = 'Ply1GameType'></radio>
+                        </div>
+                    </div>
+                </div>
+                <div class ='InputField'>
+                    <div class ='InputTitle'>Player 2</div>
+                    <div class = 'TeamFieldContainer'>
+                        <div class ='TeamField'>
+                            <div class = 'TeamSign'>X</div>
+                            <input type = 'radio' id = 'x' name = 'Ply2GameType'></radio>
+                        </div>
+                        <div class ='TeamField'>
+                            <div class ='TeamSign'>Y</div>
+                            <input type = 'radio' id = 'y' name = 'Ply2GameType'></radio>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class = 'ButtonSet'>
+                <button class = 'BackBtn'>Back</button>
+                <button class = 'ConfirmBtn'>Confirm</button>
+            </div>
+            `
+        } else if (type == 'CPU') {
+            Body.innerHTML = `
+            <div class = 'GameTitle'>Tic Tac Toe</div>
+            <div class = 'TeamFieldContainer'>
+                <div class ='TeamField'>
+                    <div class = 'TeamSign'>X</div>
+                    <input type = 'radio' id = 'x' name = 'GameType'></radio>
+                </div>
+                <div class ='TeamField'>
+                    <div class ='TeamSign'>Y</div>
+                    <input type = 'radio' id = 'y' name = 'GameType'></radio>
+                </div>
+            </div>
+            <div class = 'ButtonSet'>
+                <button class = 'BackBtn'>Back</button>
+                <button class = 'ConfirmBtn'>Confirm</button>
+            </div>
+            `
+        }
+
+        document.querySelector('.BackBtn').addEventListener('click', GameTypeSelect);
+        document.querySelector('.ConfirmBtn').addEventListener('click', () => { ConfirmGame(type) });
+    }
+
+    const ConfirmGame = (type) => {
+        if (type == 'PvP') {
+
+            const Ply1ChoiceList = document.getElementsByName('Ply1GameType');
+            const Ply2ChoiceList = document.getElementsByName('Ply2GameType');
+
+            let Ply1Choice;
+            let Ply2Choice; 
+
+            for (let i = 0; i < Ply1ChoiceList.length; i++) {
+                if (Ply1ChoiceList[i].checked) {
+                    Ply1Choice = Ply1ChoiceList[i].id;
+                }
+
+                if (Ply2ChoiceList[i].checked) {
+                    Ply2Choice = Ply2ChoiceList[i].id;
+                }
+            }
+
+            if ((Ply1Choice == Ply2Choice) || Ply1Choice == undefined || Ply2Choice == undefined) {
+                alert('You need to pick two opposing teams');
+            } else {
+                Game.Player1 = Game.PlayerCreator(Ply1Choice);
+                Game.Player2 = Game.PlayerCreator(Ply2Choice);
+
+                Body.innerHTML = '';
+
+                Render._render();
+            }
+
+            
+
+
+        } else if (type == 'CPU') {
+
+
+
+        }
+    }
+
+
+})();
+
 const Render = (() => {
     const _render = () => {
         const Body = document.querySelector('body');
@@ -34,13 +166,19 @@ const Render = (() => {
 })();
 
 const Game = (() => {
-    const Board = ['x', 'o', null,
+    const Board = [null, null, null,
                     null, null, null,
                     null, null, null
     ];
 
-    return { Board }
+    let Player1;
+    let Player2;
+
+    const PlayerCreator = team => {return {team}};
+
+    return { Board, PlayerCreator, Player1, Player2 }
 })();
+
 
 
 
